57ee43af05696a87ae37413ffedc64be6c3b9a72
Pavol Fulop <pavol.fulop@regex.sk>
2016-08-24 09:56:31 +0200

    added firebase

--- app/store/index.js
+++ app/store/index.js
@@ -1,10 +1,23 @@
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import {APIKEY,AUTHDOMAIN,DATABASEURL,STORAGEBUCKET} from '../../config.js';
+import firebase from 'firebase';
+import thunk from 'redux-thunk';
 
 export default function configureStore () {
-  const store = createStore(rootReducer);
 
-if (module.hot) {
+  const firebaseConfig = {
+    apiKey: APIKEY,
+    authDomain: AUTHDOMAIN,
+    databaseURL: DATABASEURL,
+    storageBucket: STORAGEBUCKET,
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const middleware = applyMiddleware(thunk.withExtraArgument(firebaseApp));
+
+  const store =createStore(rootReducer,middleware);
+
+  if (module.hot) {
     module.hot.accept(() => {
       const nextRootReducer = require('../reducers/index').default;
       store.replaceReducer(nextRootReducer);
